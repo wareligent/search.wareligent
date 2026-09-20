a4162292a6d5007d0ff463227a59e64ab5b143f0
wareligent <wareligent@gmail.com>
2026-09-20 22:24:03 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.WARELIGENT_CONFIG = {
-  SUPABASE_URL: "https://xveccsbdrysuiwyuvodw.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://xveccsbdrysuiwyuvodw.supabase.co",
   SUPABASE_KEY: "sb_publishable_HkyRE170ylT0kkdZxbwUSQ_ihHrS_Ra"
 };
